6b624231c38fb70880f8f97e5998269405b9fb6c
ClaudioVergara2 <claudio.vergara2@virginiogomez.cl>
2025-10-02 21:02:59 -0300

    [fix] Se mejora la apariencia del menu en mobil

--- js/mobile.js
+++ js/mobile.js
@@ -1,6 +1,13 @@
-const menuButton = document.getElementById('menuButton');
-const mobileMenu = document.getElementById('mobileMenu');
+  const menuButton = document.getElementById("menuButton");
+  const mobileMenu = document.getElementById("mobileMenu");
+  const mobileLinks = mobileMenu.querySelectorAll("a");
 
-menuButton.addEventListener('click', () => {
-   mobileMenu.classList.toggle('hidden');
-});
+  menuButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+
+  mobileLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      mobileMenu.classList.add("hidden");
+    });
+  });
